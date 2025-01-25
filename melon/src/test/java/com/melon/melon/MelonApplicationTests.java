bdee8175e6a86a10ed862b4bd527c790b7bd8fb5
parham <parhammokhtarpoor@gmail.com>
2025-01-25 05:50:25 -0800

    porject file

---
+++ melon/src/test/java/com/melon/melon/MelonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melon.melon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
